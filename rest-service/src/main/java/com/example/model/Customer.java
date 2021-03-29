de7b4c0fa6faf80973c5797da822da25a25dff4a
Jiean Yang <jiean.yang8534@gmail.com>
2021-03-29 22:25:26 +0200

    Create CustomerController and Customer UI

--- rest-service/src/main/java/com/example/model/Customer.java
+++ rest-service/src/main/java/com/example/model/Customer.java
@@ -23,6 +23,19 @@ public class Customer implements Serializable {
   @Column(name = "lastname")
   private String lastName;
 
+
+	protected Customer() {
+	}
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+  @Override
+	public String toString() {
+		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+	}
+
   // Setters, getters and constructors
   public Long getId() {
     return id;
